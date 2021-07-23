97093a1d62c5b224ef21ced976694e37c558163c
Prime38 <shoumikprime@gmail.com>
2021-07-23 16:49:33 +0600

    palindromePermutation

--- src/arraysAndStrings/PalindromePermutation.java
+++ src/arraysAndStrings/PalindromePermutation.java
@@ -1,32 +1,79 @@
 package arraysAndStrings;
 
 public class PalindromePermutation {
-    public static boolean isPalindromePermutation(String s){
-        s=s.toLowerCase();
-        s=removeAllSpaces(s);
-        int[] letterOccurrence=new int[32];
-        int len=s.length();
-        for(int i=0;i<len;i++){
-            letterOccurrence[s.charAt(i)-'a']++;
+    //book solution
+    static boolean isPermutationOfPalindrome(String phrase) {
+        int bitVector = createBitVector(phrase);
+        return bitVector == 0 || checkExactlyOneBitSet(bitVector);
+    }
+
+    /* Create a bit vector for the string. For each letter with value i, toggle the 7 * ith bit. */
+    static int createBitVector(String phrase) {
+        int bitVector = 0;
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            bitVector = toggle(bitVector, x);
         }
-        if(len%2==0){ //even length-> no character can occur odd times
-            for(int i=0;i<32;i++){
-                if(letterOccurrence[i]%2!=0) return false;
+        return bitVector;
+    }
+
+    /* Toggle the ith bit in the integer. */
+    static int toggle(int bitVector, int index) {
+        if (index < 0) return bitVector;
+        int mask = 1 << index;
+        if ((bitVector & mask) == 0) {
+            bitVector |= mask;
+        } else {
+            bitVector &= mask;
+        }
+        return bitVector;
+    }
+
+
+    /* Check that exactly one bit is set by subtracting one from the integer and ANDing it with the original integer. */
+    static boolean checkExactlyOneBitSet(int bitVector) {
+        return (bitVector & (bitVector - 1)) == 0;
+    }
+
+
+    //my solution
+    public static boolean isPalindromePermutation(String s) {
+        s = s.toLowerCase();
+        s = removeAllSpaces(s);
+        int[] letterOccurrence = new int[32];
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            letterOccurrence[s.charAt(i) - 'a']++;
+        }
+        if (len % 2 == 0) { //even length-> no character can occur odd times
+            for (int i = 0; i < 32; i++) {
+                if (letterOccurrence[i] % 2 != 0) return false;
             }
-        }else{ //odd length-> only one character can occur odd time
-            boolean oneCharWithOddOccurenceExists=false;
-            for(int i=0;i<32;i++){
-                if(letterOccurrence[i]%2!=0 && oneCharWithOddOccurenceExists==true) return false;
-                if(letterOccurrence[i]%2!=0 ) oneCharWithOddOccurenceExists=true;
+        } else { //odd length-> only one character can occur odd time
+            boolean oneCharWithOddOccurenceExists = false;
+            for (int i = 0; i < 32; i++) {
+                if (letterOccurrence[i] % 2 != 0 && oneCharWithOddOccurenceExists == true) return false;
+                if (letterOccurrence[i] % 2 != 0) oneCharWithOddOccurenceExists = true;
             }
         }
         return true;
     }
-    static String removeAllSpaces(String s){
-        int len=s.length();
-        StringBuilder stringBuilder=new StringBuilder();
-        for(int i=0;i<len;i++){
-            if(s.charAt(i)!=' '){
+
+    static int getCharNumber(Character c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    static String removeAllSpaces(String s) {
+        int len = s.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) != ' ') {
                 stringBuilder.append(s.charAt(i));
             }
         }
@@ -34,6 +81,6 @@ public class PalindromePermutation {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindromePermutation("   Ta  Act Coa    A"));
+        System.out.println(isPermutationOfPalindrome("   Ta  Act Coa    A"));
     }
 }

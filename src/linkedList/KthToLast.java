f62b82aada55290a35eea7d42cf50f5a453de468
Prime38 <shoumikprime@gmail.com>
2021-07-25 07:33:45 +0600

    kthToLast

--- src/linkedList/KthToLast.java
+++ src/linkedList/KthToLast.java
@@ -1,5 +1,36 @@
 package linkedList;
 
 public class KthToLast {
+    static Node findKthToLast(SinglyLinkedList<Integer> linkedList, int k){
+        int total=0;
+        Node cur=linkedList.head;
+        while(cur!=null){
+            total++;
+            cur=cur.next;
+        }
+        int count=0;
+        cur=linkedList.head;
+        while(count!=(total-k)){
+            count++;
+            cur=cur.next;
+        }
+        return cur;
+    }
 
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<>(1);
+        list.addNodeLast(1);
+        list.addNodeLast(2);
+        list.addNodeLast(3);
+        list.addNodeLast(4);
+        list.addNodeLast(4);
+        list.addNodeLast(5);
+        list.addNodeLast(5);
+        list.addNodeLast(1);
+        list.addNodeLast(9);
+        list.addNodeLast(8);
+        list.addNodeLast(7);
+        list.print();
+        System.out.println(findKthToLast(list,3).data);
+    }
 }
